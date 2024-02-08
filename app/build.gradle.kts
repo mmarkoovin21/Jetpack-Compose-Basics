0f338cf28d528cf23f10c5c10ca2ef51b16e2dc9
Magdalena Markovinović <mmarkovin21@foi.hr>
2024-02-08 15:08:21 +0100

    Greeting cards and on boarding screen were made.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "hr.foi.rampu.jetpackcomposebasics"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "hr.foi.rampu.jetpackcomposebasics"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
--- app/src/main/java/hr/foi/rampu/jetpackcomposebasics/MainActivity.kt
+++ app/src/main/java/hr/foi/rampu/jetpackcomposebasics/MainActivity.kt
@@ -3,13 +3,28 @@ package hr.foi.rampu.jetpackcomposebasics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ComposableOpenTarget
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import hr.foi.rampu.jetpackcomposebasics.ui.theme.JetpackComposeBasicsTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,13 +32,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackComposeBasicsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                MyApp(modifier = Modifier.fillMaxSize())
             }
         }
     }
@@ -31,16 +40,84 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    var expanded by remember {
+        mutableStateOf(false)
+    }
+    var extraPadding = if (expanded) 48.dp else 0.dp
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
+        modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+        ){
+        Row(modifier = modifier.padding(24.dp)){
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding)
+            ) {
+                Text(text = "Hello")
+                Text (text = "$name!")
+            }
+            ElevatedButton(
+                onClick = { expanded = !expanded }
+            ) {
+                Text(if (expanded)"Show less" else "Show more")
+            }
+        }
+    }
 }
-
-@Preview(showBackground = true)
+@Composable
+fun Greetings(
+    modifier: Modifier = Modifier,
+    names: List<String> = listOf("Megi", "David", "Dario", "Marin")
+){
+    Column {
+        for (name in names){
+            Greeting(name = name)
+        }
+    }
+}
+@Composable
+fun MyApp(
+    modifier: Modifier = Modifier
+){
+    Surface(
+        modifier = modifier,
+        color = MaterialTheme.colorScheme.background
+    ) {
+        var shouldShowOnboarding by remember { mutableStateOf(true) }
+        if(shouldShowOnboarding){
+            OnBoardingScreen(onContinueClicked = { shouldShowOnboarding = false})
+        }else{
+            Greetings()
+        }
+    }
+}
+@Composable
+fun OnBoardingScreen(
+    onContinueClicked: () -> Unit,
+    modifier: Modifier = Modifier
+){
+    var shouldShowOnboarding by remember {
+        mutableStateOf(true)
+    }
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text("Welcome to the Basics Codelab")
+        Button(
+            modifier = Modifier.padding(vertical = 24.dp),
+            onClick = onContinueClicked
+        ) {
+            Text(text = "Continue")
+        }
+    }
+}
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
 @Composable
 fun GreetingPreview() {
     JetpackComposeBasicsTheme {
-        Greeting("Android")
+        OnBoardingScreen(onContinueClicked = {})
     }
 }
