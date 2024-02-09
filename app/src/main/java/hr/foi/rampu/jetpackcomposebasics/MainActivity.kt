41756d632d9c5639db537c3d909c573cd466e716
Magdalena Markovinović <mmarkovin21@foi.hr>
2024-02-09 14:17:27 +0100

    Creating animations, changing colors in theme.kt file.
    Codelab: Write your fires Compose App is finished.

--- app/src/main/java/hr/foi/rampu/jetpackcomposebasics/MainActivity.kt
+++ app/src/main/java/hr/foi/rampu/jetpackcomposebasics/MainActivity.kt
@@ -1,28 +1,38 @@
 package hr.foi.rampu.jetpackcomposebasics
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.material3.Button
-import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.ComposableOpenTarget
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import hr.foi.rampu.jetpackcomposebasics.ui.theme.JetpackComposeBasicsTheme
@@ -40,27 +50,50 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    var expanded by remember {
+    var expanded by rememberSaveable {
         mutableStateOf(false)
     }
-    var extraPadding = if (expanded) 48.dp else 0.dp
     Surface(
         color = MaterialTheme.colorScheme.primary,
         modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
         ){
-        Row(modifier = modifier.padding(24.dp)){
+        Row(modifier = modifier
+            .padding(12.dp)
+            .animateContentSize(
+                animationSpec = tween(
+                    durationMillis = 350,
+                    easing = FastOutSlowInEasing
+                )
+            )){
             Column(
                 modifier = Modifier
                     .weight(1f)
-                    .padding(bottom = extraPadding)
+                    .padding(12.dp)
             ) {
-                Text(text = "Hello")
-                Text (text = "$name!")
+                Text(text = "Hello,")
+                Text (
+                    text = name,
+                    style = MaterialTheme.typography.headlineMedium.copy(
+                        fontWeight = FontWeight.ExtraBold
+                        )
+                    )
+                if(expanded) {
+                    Text(
+                        text = ("Composem ipsum color sit lazy, " +
+                                "padding theme elit, sed do bouncy.").repeat(4)
+                    )
+                }
             }
-            ElevatedButton(
+            IconButton(
                 onClick = { expanded = !expanded }
             ) {
-                Text(if (expanded)"Show less" else "Show more")
+                Icon(
+                    imageVector = if(expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                    if (expanded)
+                        stringResource(R.string.show_less)
+                    else
+                        stringResource(R.string.show_more)
+                )
             }
         }
     }
@@ -68,10 +101,10 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun Greetings(
     modifier: Modifier = Modifier,
-    names: List<String> = listOf("Megi", "David", "Dario", "Marin")
+    names: List<String> = List(30) {"$it"}
 ){
-    Column {
-        for (name in names){
+    LazyColumn(modifier =  modifier.padding(vertical = 4.dp)) {
+        items(items = names){ name ->
             Greeting(name = name)
         }
     }
@@ -84,20 +117,20 @@ fun MyApp(
         modifier = modifier,
         color = MaterialTheme.colorScheme.background
     ) {
-        var shouldShowOnboarding by remember { mutableStateOf(true) }
+        var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
         if(shouldShowOnboarding){
-            OnBoardingScreen(onContinueClicked = { shouldShowOnboarding = false})
+            OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false})
         }else{
             Greetings()
         }
     }
 }
 @Composable
-fun OnBoardingScreen(
+fun OnboardingScreen(
     onContinueClicked: () -> Unit,
     modifier: Modifier = Modifier
 ){
-    var shouldShowOnboarding by remember {
+    var shouldShowOnboarding by rememberSaveable {
         mutableStateOf(true)
     }
     Column(
@@ -114,10 +147,23 @@ fun OnBoardingScreen(
         }
     }
 }
-@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+//@Preview(
+//    showBackground = true,
+//    widthDp = 320,
+//    uiMode = UI_MODE_NIGHT_YES,
+//    name = "GreetingsDarkPreview"
+//)
+@Preview(showBackground = true, widthDp = 320)
 @Composable
 fun GreetingPreview() {
     JetpackComposeBasicsTheme {
-        OnBoardingScreen(onContinueClicked = {})
+        Greetings()
     }
 }
+//@Preview(showBackground = true, widthDp = 320)
+//@Composable
+//fun OnBoardingPreview() {
+//    JetpackComposeBasicsTheme {
+//        OnboardingScreen(onContinueClicked = {})
+//    }
+//}
